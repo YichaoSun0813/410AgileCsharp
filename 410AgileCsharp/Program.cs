16df716334855d591e7365db309da29fc72aa4c0
Roman Bogushevich <romanbog78@gmail.com>
2020-07-01 20:20:37 -0700

    Add project files.

---
+++ 410AgileCsharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _410AgileCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
