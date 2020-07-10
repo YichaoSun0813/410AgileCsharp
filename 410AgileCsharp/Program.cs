503c23076c3851355662b82a6d13cf589a564db6
Roman Bogushevich <romanbog78@gmail.com>
2020-07-09 18:46:43 -0700

    Added a call to list directory on remote server after logon. FtpHandler.logOn() is now starting to work!

--- 410AgileCsharp/Program.cs
+++ 410AgileCsharp/Program.cs
@@ -13,17 +13,25 @@ namespace _410AgileCsharp
             //Prompt for FTP URL, and add into FtpWebRequest.
             //Keep in mind that this needs to be a full URL, which should look something like this: ftp://HostName.com/
             //This also initializes FtpWebRequest, which is kinda major. Maybe we should find a way to move this? 
+            //Maybe mainHandler constructor?
             Console.Write("Enter an FTP server URL: ");
             string url = Console.ReadLine();
             mainHandler.mainRequest = (FtpWebRequest)WebRequest.Create(url);
 
             //HUGE: with EnableSsl = false, your username and password will be transmitted over the network in cleartext.
-            //Please don't transmit your username and password over the network in cleartext
+            //Please don't transmit your username and password over the network in cleartext :)
             mainHandler.mainRequest.EnableSsl = true;
 
-            mainHandler.logOn();
+            if (mainHandler.logOn())
+            {
+                Console.WriteLine("logOn successfull");
+            }
+            else
+            {
+                Console.WriteLine("logOn unsuccessfull");
+            }
 
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
         }
     }
 }
