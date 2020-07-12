dfe0b89456bf2bbac564080061aab219c57552a7
Ted <Dukekamono@hotmail.com>
2020-07-12 14:50:44 -0700

    Using StreamReader. Is this what we want?

---
+++ 410AgileCsharp/FileUploader.cs
@@ -0,0 +1,25 @@
+﻿//Resources https://docs.microsoft.com/en-us/dotnet/standard/io/how-to-read-text-from-a-file
+
+using System;
+using System.IO;
+
+namespace _410AgileCsharp
+{
+	class FileUploader
+	{
+		// example filePath: @"\\IPAddress or remote machine Name\ShareFolder\FileName.txt"
+		public void Load(string filePath)
+		{
+			try
+			{
+				using StreamReader str = new StreamReader(filePath);
+				var line = str.ReadToEnd();
+				Console.WriteLine(line);
+			}
+			catch (IOException ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+		}
+	}
+}
