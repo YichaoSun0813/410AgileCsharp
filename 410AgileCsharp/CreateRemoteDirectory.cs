a9491cbbf1369841904f7fb82a4b3881e316b405
Sam Youngs <youngs3@pdx.edu>
2020-07-20 20:22:15 -0700

    1st commit

---
+++ 410AgileCsharp/CreateRemoteDirectory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.IO;
+namespace CreateRemoteDirectory
+{
+    class RemoteMkDir
+    {
+        public FtpWebRequest FtpWebRequest;
+        public FtpWebResponse ftpWebResponse;
+
+        public bool MkDirRemote()
+        {
+            try
+            {
+
+                FtpWebRequest = (FtpWebRequest)WebRequest.Create("ftp://test.rebex.net/");
+                FtpWebRequest.Method = WebRequestMethods.Ftp.MakeDirectory;
+
+
+            }
+            catch
+            {
+
+            }
+        }
+
+    }
+}
