30a59ac128984bd7b94f251a141f295324a66555
Sam Youngs <youngs3@pdx.edu>
2020-08-05 22:03:24 -0700

    created new file

---
+++ 410AgileCsharp/CopyRemoteDirectories.cs
@@ -0,0 +1,16 @@
+﻿using _410AgileCsharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CopyRemote
+{
+    class CopyRemoteDirectory
+    {
+        public bool Copy(FtpHandler credentials)
+        {
+            return true;
+        }
+
+    }
+}
