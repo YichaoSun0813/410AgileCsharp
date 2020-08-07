331d15240e0e653023b1954806c815c37b3637b7
Roman Bogushevich <romanbog78@gmail.com>
2020-08-06 19:11:17 -0700

    slightly refactored SavedConnectionHandler.ReadAll() created SavedConnectionHandler.Connect()

--- 410AgileCsharp/SavedConnectionHandler.cs
+++ 410AgileCsharp/SavedConnectionHandler.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
+using System.Security;
+using System.Net;
 
 namespace _410AgileCsharp
 {
@@ -17,13 +20,11 @@ namespace _410AgileCsharp
             //read from text file, load into an array of SavedConnections. Display entire array at the end. 
             try
             {
-                using(var sr = new StreamReader("SavedConnections.txt"))
+                using var sr = new StreamReader("SavedConnections.txt");
+                for (int i = 0; !sr.EndOfStream; i++)
                 {
-                    for(int i = 0; !sr.EndOfStream; i++)
-                    {
-                        connectionList[i].url = sr.ReadLine();
-                        connectionList[i].userName = sr.ReadLine();
-                    }
+                    connectionList[i].url = sr.ReadLine();
+                    connectionList[i].userName = sr.ReadLine();
                 }
             }
             catch(IOException e)
@@ -42,7 +43,99 @@ namespace _410AgileCsharp
         {
             //Allow user to select from array of SavedConnections, and feed information into appropriate FtpHandler fields.
             //prompt for password
-            return true;
+            try
+            {
+                //print out all possible connections
+                for(int i = 0; i < connectionList.Length; i++)
+                {
+                    Console.WriteLine(i);
+                    Console.WriteLine("url: " + connectionList[i].url);
+                    Console.WriteLine("username: " + connectionList[i].userName);
+                }
+
+                //prompt user for a selection
+                Console.Write("Make a selection: ");
+                string userInput = Console.ReadLine();
+                int userResult;
+                for (; ; )
+                {
+                    try
+                    {
+                        userResult = Int32.Parse(userInput);
+                        if(userResult < 0 || userResult > connectionList.Length)
+                        {
+                            throw new Exception();
+                        }
+                        break;
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Cannot parse. Please try again!");
+                        Console.WriteLine(": ");
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Input out of range. Try again!");
+                        Console.WriteLine(": ");
+                    }
+                }
+
+                //feed connectionList result into our FtpHandler
+                toConnect.url = connectionList[userResult].url;
+                toConnect.currentUser = connectionList[userResult].userName;
+
+                //the rest is copy paste from logonunsaved
+
+
+                toConnect.currentPass = new SecureString();
+                ConsoleKeyInfo key;
+
+                Console.Write("Enter password: ");
+                do
+                {
+                    key = Console.ReadKey(true);
+
+                    // Ignore any key out of range.
+                    if (((int)key.Key) >= 65 && ((int)key.Key <= 90))
+                    {
+                        // Append the character to the password.
+                        toConnect.currentPass.AppendChar(key.KeyChar);
+                        Console.Write("*");
+                    }
+                    // Exit if Enter key is pressed.
+                } while (key.Key != ConsoleKey.Enter);
+                Console.WriteLine();
+
+
+                //Now, we feed all of these into a NetworkCredentials class, and feed that into our FtpWebRequest
+                toConnect.mainRequest.Credentials = new NetworkCredential(toConnect.currentUser, toConnect.currentPass);
+
+                //Other parameters that are important to have
+                toConnect.mainRequest.KeepAlive = false;
+                toConnect.mainRequest.UseBinary = true;
+                toConnect.mainRequest.UsePassive = true;
+
+                //List all files in directory, for testing, and to make sure we're connected
+                toConnect.mainRequest.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+                //Assign to mainResponse
+                toConnect.mainResponse = (FtpWebResponse)toConnect.mainRequest.GetResponse();
+                //Read the response stream
+                Stream responseStream = toConnect.mainResponse.GetResponseStream();
+                StreamReader responseRead = new StreamReader(responseStream);
+                //Print
+                Console.WriteLine(responseRead.ReadToEnd());
+
+                //gracefully exit reader and response
+                responseRead.Close();
+                toConnect.mainResponse.Close();
+                return true;
+                
+            }
+            catch(Exception bruh)
+            {
+                Console.WriteLine(bruh.Message);
+                return false;
+            }
         }
 
         public bool SaveConnection(FtpHandler toSave)
