d5a2124e781f5843e002e71602e054f36b00906a
Roman Bogushevich <romanbog78@gmail.com>
2020-08-06 18:33:01 -0700

    Finished SavedConnectionHandler.ReadAll()

--- 410AgileCsharp/SavedConnectionHandler.cs
+++ 410AgileCsharp/SavedConnectionHandler.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace _410AgileCsharp
 {
     struct SavedConnection{
-        string url;
-        string userName;
+        public string url;
+        public string userName;
     }
     class SavedConnectionHandler
     {
@@ -14,7 +15,27 @@ namespace _410AgileCsharp
         public void ReadAll()
         {
             //read from text file, load into an array of SavedConnections. Display entire array at the end. 
-
+            try
+            {
+                using(var sr = new StreamReader("SavedConnections.txt"))
+                {
+                    for(int i = 0; !sr.EndOfStream; i++)
+                    {
+                        connectionList[i].url = sr.ReadLine();
+                        connectionList[i].userName = sr.ReadLine();
+                    }
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("File could not be opened.");
+                Console.WriteLine(e.Message);
+            }
+            catch(Exception flip)
+            {
+                Console.WriteLine("Unexpected exception while reading saved connection file");
+                Console.WriteLine(flip.Message);
+            }
         }
 
         public bool Connect(FtpHandler toConnect)
