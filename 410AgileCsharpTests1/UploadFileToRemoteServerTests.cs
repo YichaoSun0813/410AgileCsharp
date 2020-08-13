414b1a5b94dc93e1446237c8417c4a9e74377430
Ted <Dukekamono@hotmail.com>
2020-08-13 11:10:17 -0700

    Rest of my unit tests (James)

--- 410AgileCsharpTests1/UploadFileToRemoteServerTests.cs
+++ 410AgileCsharpTests1/UploadFileToRemoteServerTests.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using _410AgileCsharp;
-using UploadFileToRemoteServer;
+﻿using _410AgileCsharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Net;
+using UploadFileToRemoteServer;
 
-namespace ListFilesOnRemoteServer.Tests
+namespace UploadFileToRemoteServerTests.Tests
 {
-    [TestClass()]
+	[TestClass()]
     public class UploadFileToRemoteServerTests
     {
         [TestMethod()]
@@ -21,6 +21,23 @@ namespace ListFilesOnRemoteServer.Tests
 			var upload = new Upload();
             //Act
             var response = upload.UploadToRemote(mainRequest, "SavedConnections.txt");
+            //Assert
+            Assert.IsTrue(response);
+        }
+
+        [TestMethod()]
+        public void UploadMultipleFilesToRemoteServer_Upload_SuccesfulResult()
+        {
+            //Arrange
+            var mainRequest = new FtpHandler
+            {
+                url = "ftp://13.59.40.218/",
+                userName = "ftp_user",
+                securePwd = new NetworkCredential("", "Kamono123").SecurePassword
+            };
+            var upload = new Upload();
+            //Act
+            var response = upload.UploadMultipleToRemote(mainRequest, "SavedConnections.txt;410AgileCsharp.runtimeconfig.json");
             //Assert
             Assert.IsTrue(response);
         }
