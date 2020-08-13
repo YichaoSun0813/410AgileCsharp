9b68f3e183e25671d68e4f9d4e44a8ac85204b22
Roman Bogushevich <romanbog78@gmail.com>
2020-08-13 10:39:35 -0700

    Added SaveConnectionInfoTests

---
+++ 410AgileCsharpTests1/SaveConnectionInfoTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using _410AgileCsharp;
+using System.IO;
+
+namespace _410AgileCsharpTests1
+{
+    [TestClass()]
+    public class SaveConnectionInfoTests
+    {
+        [TestMethod()]
+        public void TestSavingInfo()
+        {
+            var mainRequest = new FtpHandler();
+            mainRequest.url = "ftp://13.59.40.218/";
+            mainRequest.userName = "ftp_user";
+            mainRequest.securePwd = new NetworkCredential("", "Kamono123").SecurePassword;
+            mainRequest.SaveInfo();
+            Assert.IsTrue(File.ReadAllText("SavedConnections.txt").Contains("ftp_user"));
+        }
+    }
+}
