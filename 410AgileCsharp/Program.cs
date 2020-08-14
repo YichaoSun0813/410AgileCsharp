9694e9908011787d1fd2e159b9cce6822b4f0eb8
YichaoSun0813 <59580001+YichaoSun0813@users.noreply.github.com>
2020-08-13 21:11:32 -0700

    Update Program.cs

--- 410AgileCsharp/Program.cs
+++ 410AgileCsharp/Program.cs
@@ -1,4 +1,4 @@
-﻿using CreateRemoteDirectory;
+using CreateRemoteDirectory;
 using DeleteFileFromRemoteServer;
 using DownloadFileFromRemoteServer;
 using ListFilesOnRemoteServer;
@@ -12,15 +12,18 @@ namespace _410AgileCsharp
 {
 	class Program
 	{
+		
 		private static Timer timer;
 		private static void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
 		{
 			Console.WriteLine("Timeout！");
 			System.Environment.Exit(0);
 		}
-		
-		static void Main(string[] args)
+
+        [Obsolete]
+        static void Main(string[] args)
 		{
+			string [] logHistory = new string[101];
 			FtpHandler mainHandler = new FtpHandler();
 			RemoteLS listRemote = new RemoteLS();
 			RemoteMkDir mkDirRemote = new RemoteMkDir();
@@ -29,17 +32,43 @@ namespace _410AgileCsharp
 			Rename rename = new Rename();
 			Delete delete = new Delete();
 
+
+			string[] lines = System.IO.File.ReadAllLines(@".\log.txt");
+
+			int t;
+
+
+				string[] number = System.IO.File.ReadAllLines(@".\number.txt");
+				Console.WriteLine(number);
+				t = Int32.Parse(number[0]);
+			
+			
+			int i = 0;
+			foreach (string line in lines)
+			{
+				if (i < t)
+                {
+					logHistory[i] = String.Copy(line);
+					i++;
+				}
+			}
+
+			Console.WriteLine("Log history has been loaded.\n");
+
 			mainHandler.LogOnInitial();
+			logHistory[i] = DateTime.Now.ToString() + "  log in";	
+			i++;
+			t++;
 
 			try
 			{
 				bool loop = true;
 				while (loop)
 				{
-					Console.WriteLine("\nEnter a command:\nls = List Directory Details\nupload = UpLoad File\nupload-m = Upload Multiple files\ndownload = DownLoad File\ndelete = Delete a File\nrr = Rename a Remote File\nmkdir = Make a Remote Directory\nsave = Save currenly connected server \nd = Disconnect\n");
+					Console.WriteLine("\nEnter a command:\nls = List Directory Details\nupload = UpLoad File\nupload-m = Upload Multiple files\ndownload = DownLoad File\ndelete = Delete a File\nrr = Rename a Remote File\nmkdir = Make a Remote Directory\nsave = Save currenly connected server \nlog = Log History\nd = Disconnect\n");
 					
 					timer = new System.Timers.Timer();
-					timer.Interval = 10000;
+					timer.Interval = 30000;
 					timer.Elapsed += OnTimedEvent;
 					timer.AutoReset = true;
 					timer.Enabled = true;
@@ -53,21 +82,33 @@ namespace _410AgileCsharp
 							Console.WriteLine("Enter the filename to put on the ftp.\n");
 							string upFile = Console.ReadLine();
 							upload.UploadToRemote(mainHandler, upFile);
+							logHistory[i] = DateTime.Now.ToString() + "  attempted to upload " + upFile;
+							i++;
+							t++;
 							break;
 						case "upload-m":
 							Console.WriteLine("Enter the filename to put on the ftp seperated by \";\"'s.\n");
 							string upFiles = Console.ReadLine();
 							upload.UploadMultipleToRemote(mainHandler, upFiles);
+							logHistory[i] = DateTime.Now.ToString() + "  attempted to upload " + upFiles;
+							i++;
+							t++;
 							break;
 						case "download":
 							Console.WriteLine("Enter filename to take from the ftp.\n");
 							string downFile = Console.ReadLine();
 							download.DownloadFromRemote(mainHandler, downFile);
+							logHistory[i] = DateTime.Now.ToString() + "  attempted to download " + downFile;
+							i++;
+							t++;
 							break;
 						case "delete":
 							Console.WriteLine("Enter filename to DELETE from the ftp.\n");
 							string deleteFile = Console.ReadLine();
 							delete.DeleteRemoteFile(mainHandler, deleteFile);
+							logHistory[i] = DateTime.Now.ToString() + "  attempted to delete " + deleteFile;
+							i++;
+							t++;
 							break;
 						case "ls":
 							listRemote.ListRemote(mainHandler);
@@ -78,6 +119,9 @@ namespace _410AgileCsharp
 							Console.WriteLine("Enter the new name.\n");
 							string newFileName = Console.ReadLine();
 							rename.RenameRemoteFile(mainHandler, currnetFile, newFileName);
+							logHistory[i] = DateTime.Now.ToString() + "  attempted to rename " + currnetFile + " to " + newFileName;
+							i++;
+							t++;
 							break;
 						case "d":
 							loop = false;
@@ -86,9 +130,18 @@ namespace _410AgileCsharp
 							Console.WriteLine("Enter directory name. \n");
 							string dir = Console.ReadLine();
 							mkDirRemote.MkDirRemote(mainHandler, dir);
+							logHistory[i] = DateTime.Now.ToString() + "  attempted to make a directory called " + dir;
+							i++;
+							t++;
 							break;
 						case "save":
 							mainHandler.SaveInfo();
+							break;
+						case "log":
+							for (int j = 0; j < i; j++)
+                            {
+								Console.WriteLine(logHistory[j]);
+                            }
 							break;
 						default:
 							Console.WriteLine("Wrong Command.\n");
@@ -96,8 +149,23 @@ namespace _410AgileCsharp
 					}
 					command = string.Empty;
 				}
+
 				Console.WriteLine("Press enter to disconnect");
 				Console.ReadLine();
+
+				logHistory[i] = DateTime.Now.ToString() + "  log out";
+				i++;
+				t++;
+
+				string [] tt = { t.ToString() };
+				System.IO.File.WriteAllLines(@".\number.txt", tt);
+				
+				for (int j = 0; j < i; j++)
+                {
+					System.IO.File.WriteAllLines(@".\log.txt", logHistory);
+				}
+				Console.WriteLine("Log history has been saved.\n");
+
 				mainHandler.LogOff();
 			}
 			catch (Exception ex)
@@ -108,3 +176,7 @@ namespace _410AgileCsharp
 		}
 	}
 }
+
+
+
+
